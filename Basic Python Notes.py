1d78a860d489611119689697ae8b9fd5d60db6c4
GerardoRodriguz <31050428+GerardoRodriguz@users.noreply.github.com>
2017-11-17 15:25:56 -0800

    Initial commit

---
+++ Basic Python Notes.py
@@ -0,0 +1,21 @@
+print("Hello World")
+print("Welcome To Class")
+
+# This is how you add a comment
+# They are awesome
+''' This will not appear at
+all, three quotations marks '''
+
+
+
+
+
+
+# Variables
+a = "Hello World"
+print(a)
+
+b = 5
+c = 7
+print(b + c)
+print(-1 - (-5))
